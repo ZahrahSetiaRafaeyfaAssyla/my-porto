ecedd5053d38a2a9228f1a54e1b968965aaa798c
ZahrahSetiaRafaeyfaAssyla <zahrahrafaeyfa@gmail.com>
2025-08-28 06:47:26 +0700

    first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DarkVeil from "./Backgrounds/DarkVeil/darkveil";
+import DarkVeil from "./Backgrounds/DarkVeil/DarkVeil";
 import LoadingScreen from "./Components/LoadingScreen";
 import PillNav from "./Components/PillNav/pillnav";
 import TrueFocus from "./TextAnimations/TrueFocus/TrueFocus";
